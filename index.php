7c93b4d1f70772336339c80c8d764f93bc128173
vikram <pariharvikram1989@gmail.com>
2015-03-17 01:01:38 +0530

    added css

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, max-scale=1.0"/>
     <title>Nike Emailer</title>
-    <link rel="stylesheet" type="text/css" href="../css/pure-min.css">
-    <link rel="stylesheet" type="text/css" href="../css/grids-responsive-min.css">
-    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/pure-min.css">
+    <link rel="stylesheet" type="text/css" href="css/grids-responsive-min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
 
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css" />
 
